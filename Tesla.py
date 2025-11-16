bea57a8b0098ffb6982ff00ff48d2d13efb251d0
Riophat <nguyenquocthienphat@gmail.com>
2025-11-17 05:50:16 +0700

    Add files via upload

---
+++ Tesla.py
@@ -0,0 +1,9 @@
+print("""I'm so bored
+what i just learned from a video on youtube is that
+Boredom is just a normal feeling of human
+it doesn't represent for why we are bored,
+it says that the work we are having is not so appealing
+but, there are some benefits from this
+when we are bored, we tend to think about the future
+so, just a moment in a bus, it's your decision if to pull your phone out and stay active
+you can stay bored to become creative is great.""")
